523383610945b262728cd51a17be0f2556fd6a69
Kristiyan Vachev <typhon04@gmail.com>
2016-08-11 22:56:18 +0300

    refactoring Projectile.js

--- js/Projectile.js
+++ js/Projectile.js
@@ -2,7 +2,6 @@
  * Created by Kristiyan Vachev on 11-Aug-16.
  */
 
-
 var x,
     y,
     speed = 5,
@@ -12,18 +11,9 @@ var x,
 
 function Projectile() {
     console.log("created projectile");
-    var _this = this;
 
     canShoot = true;
     collided = false;
-
-    // constructor
-    (function () {
-        _this.x = null;
-        _this.y = null;
-
-    })();
-
 }
 
 Projectile.prototype.init = function (startX, startY, invadersArr) {
@@ -32,34 +22,26 @@ Projectile.prototype.init = function (startX, startY, invadersArr) {
         canShoot = false;
 
         x = startX;
-        y = startY;
+        y = startY - 20;
         invadersArray = invadersArr;
         animateProjectile();
     }
 };
 
-
 function animateProjectile() {
-    //clear previous
-    ctx.clearRect(x, y + speed, 3, 15);
+    ctx.clearRect(x, y + speed, 4, 15);
 
-    ctx.fillRect(x, y, 3, 15);
+    ctx.fillRect(x, y, 4, 15);
     y = y - speed;
 
     for (var i = 0; i < invadersArray.length; i += 1) {
-        //check if the it hits
-        console.log(invadersArray.length);
 
         if (x >= invadersArray[i].x && x <= (invadersArray[i].x + invadersArray[i].width)) {
             if (y >= invadersArray[i].y && y <= (invadersArray[i].y + invadersArray[i].height)) {
                 collided = true;
-
                 invadersArray.splice(i, 1);
             }
-
         }
-
-        //if it does, remove element from array
     }
 
     if (y > 0 && !collided) {
@@ -69,7 +51,5 @@ function animateProjectile() {
         canShoot = true;
         collided = false;
     }
-
-
 }
 
